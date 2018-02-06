942bfeee11d44db2187d5d2760c06cf9cebe9c62
Deividhy <deividhytonetti@gmail.com>
2018-02-06 19:06:02 -0200

    Select Esta dando erro

--- class/Usuario.php
+++ class/Usuario.php
@@ -71,6 +71,44 @@ class Usuario
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 		));
 	}
+
+	public static function getList()
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login)
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH' => "%".$login."%")); // usa esse %% para buscar todos os dados que tem por exemplo um letra "o"
+	}
+
+	public function login($login, $password)
+	{
+		$sql = new Sql();
+	
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = : PASSWORD", array(
+			":LOGIN"=>$login, 
+			":PASSWORD"=>$password
+		));
+
+		if(count($results) > 0)
+		{
+			$row = $results[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+		else
+		{
+			throw new Exception("Login e/ou senha inválidos", 1);
+			
+		}
+	}
 }
 
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,23 @@ require_once("config.php");
 //$usuarios = $sql-> select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
-$root = new Usuario();
-$root -> loadById(3);
+//Aqui em baixo carrega  1 usuário
+//$root = new Usuario();
+//$root -> loadById(3);
+//echo $root;
 
-echo $root;
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+// carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("o");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha;
+$verifica = new Usuario();
+$verifica->login("root", "123");
+
+echo $verifica;
 ?>
